563b30f437529c34dbb7de8656b38fa603353c49
arielhung <37277397+arielhung@users.noreply.github.com>
2018-03-11 16:59:39 -0700

    5 steps of TipPro

---
+++ TipPro/TipPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipPro
+//
+//  Created by Ariel Hung on 11/03/2018.
+//  Copyright © 2018 AH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
